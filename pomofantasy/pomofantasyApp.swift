91616c008e94703fb63b773b103b935525ebd849
kevinzeladacl <iam@kevinzelada.cl>
2025-11-19 00:15:11 -0300

    Initial Commit

---
+++ pomofantasy/pomofantasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomofantasyApp.swift
+//  pomofantasy
+//
+//  Created by mri on 19-11-25.
+//
+
+import SwiftUI
+
+@main
+struct pomofantasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
